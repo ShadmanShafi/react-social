8702d631bfa8b9968fdda72447f42522268dc1df
Shadman Shafi <shadmanshafi@iut-dhaka.edu>
2022-05-20 22:06:13 +0600

    refactored

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -38,9 +38,10 @@ const Navbar = () => {
 
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
-      <Link to="/" className={classes.brandContainer}>
-        <img component={Link} to="/" src={memoriesText} alt="icon" height="45px" />
-        <img className={classes.image} src={memoriesLogo} alt="icon" height="40px" />
+      <p className="navbartitle">React Social - Image Social Media App </p>
+      <Link to="/" className={classes.brandContainer}> 
+        {/* <img component={Link} to="/" src={memoriesText} alt="icon" height="45px" /> */}
+        {/* <img className={classes.image} src={memoriesLogo} alt="icon" height="40px" /> */}
       </Link>
       <Toolbar className={classes.toolbar}>
         {user?.result ? (
--- client/src/components/PostDetails/PostDetails.jsx
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -58,8 +58,8 @@ const Post = () => {
             </Link>
           </Typography>
           <Typography variant="body1">{moment(post.createdAt).fromNow()}</Typography>
-          <Divider style={{ margin: '20px 0' }} />
-          <Typography variant="body1"><strong>Realtime Chat - coming soon!</strong></Typography>
+          {/* <Divider style={{ margin: '20px 0' }} />
+          <Typography variant="body1"><strong>Realtime Chat - coming soon!</strong></Typography> */}
           <Divider style={{ margin: '20px 0' }} />
           <CommentSection post={post} />
           <Divider style={{ margin: '20px 0' }} />
